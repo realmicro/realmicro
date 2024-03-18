6d4a0d433613dc01776f1fd4cac40c3a893cdaeb
reezhou <reechou@gmail.com>
2024-03-18 17:25:31 +0800

    add feature: api gateway

--- examples/api/main.go
+++ examples/api/main.go
@@ -13,9 +13,14 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	reg := etcd.NewRegistry(registry.Addrs([]string{"127.0.0.1:2379"}...))
+	reg := etcd.NewRegistry(
+		registry.Addrs([]string{"127.0.0.1:2379"}...),
+	)
 
-	srv := api.NewApi(api.WithRegistry(reg))
+	srv := api.NewApi(
+		api.WithRegistry(reg),
+		api.WithAddress(":9090"),
+	)
 
 	if err := srv.Run(ctx); err != nil {
 		logger.Fatal(err)
