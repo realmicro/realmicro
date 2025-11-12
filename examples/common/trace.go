7fcbbe7f933a7c4415aad3de2e4671f001cac5f6
reezhou <reechou@gmail.com>
2025-11-12 17:04:39 +0800

    add: trace example

--- examples/common/trace.go
+++ examples/common/trace.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"context"
+	"net"
 
 	"github.com/realmicro/realmicro/logger"
 	"go.opentelemetry.io/otel"
@@ -12,6 +13,20 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
+
+func GetLocalIp() string {
+	addrs, err := net.InterfaceAddrs()
+	if err == nil {
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					return ipnet.IP.String()
+				}
+			}
+		}
+	}
+	return "127.0.0.1"
+}
 
 func NewTraceProvider(ctx context.Context, endpoint, token, serverName string) (*trace.TracerProvider, error) {
 	opts := []otlptracegrpc.Option{
@@ -27,7 +42,7 @@ func NewTraceProvider(ctx context.Context, endpoint, token, serverName string) (
 		resource.WithAttributes(
 			attribute.KeyValue{Key: "token", Value: attribute.StringValue(token)},             // <token>替换为业务系统Token
 			attribute.KeyValue{Key: "service.name", Value: attribute.StringValue(serverName)}, // <serviceName>替换为应用名
-			attribute.KeyValue{Key: "host.name", Value: attribute.StringValue("<hostName>")},  // <hostName>替换为IP地址
+			attribute.KeyValue{Key: "host.name", Value: attribute.StringValue(GetLocalIp())},  // <hostName>替换为IP地址
 		),
 	}...)
 	if err != nil {
